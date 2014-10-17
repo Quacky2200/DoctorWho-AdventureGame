c9a993c22da2a5d1ad71670ae5fe5affbd85c027
Matthew James <Quacky2200@hotmail.com>
2014-10-17 04:16:38 +0100

    Various updates
    Includes updated converses, importing characters with items,
    synchronised rooms using a dictionary matrix instead of a
    list-dictionary matrix. Automatic import from readstory.py

--- readitems.py
+++ readitems.py
@@ -1,10 +1,8 @@
 import xml.etree.ElementTree as ET
 from player import *
 
-def return_items():
-	tree = ET.parse('items.xml')
-	xml = tree.getroot()
-	for xmlitems in xml:
+def return_items(sentxmlitems):
+	for xmlitems in sentxmlitems:
 		tempitem = {'description': xmlitems.text}
 		for attribute, value in xmlitems.items():
 			tempitem[attribute] = (value if not (value.lower() == 'true') and not (value.lower() == 'false') else (True if value.lower() == 'true' else False))
@@ -16,36 +14,8 @@ def return_item(item_id):
 	if item_exists(item_id):
 		return list([item for item in inventory if item['name'].replace('_', '') == item_id])[0]
 def weight_of_items(items):
-    """This function takes a list of items and returns the total weight 
-    from the whole list. Some of the items will be in grams and kilograms 
-    and need to be translated into KG for the final total. The expected
-    return result is a string. For Example:
-
-    >>> weight_of_items([item_jelly_babies, item_psychic_paper])
-    0.015
-
-    >>> weight_of_items([])
-    0
-
-    """
     return sum([float(i['weight'].lower().replace('kg', '')) if 'kg' in i['weight'].lower() else float(i['weight'].lower().replace('g','')) / 1000 for i in items])
 def list_of_items(items):
-    """This function takes a list of items (see items.py for the definition) and
-    returns a comma-separated list of item names (as a string). For example:
-
-    >>> list_of_items([item_jelly_babies, item_psychic_paper])
-    'psychic paper and a silver tin containing jelly babies'
-
-    >>> list_of_items([item_sonic_screwdriver])
-    'a sonic screwdriver'
-
-    >>> list_of_items([])
-    ''
-
-    >>> list_of_items([item_money, item_handbook, item_laptop])
-    'some money, a student handbook and a laptop'
-
-    """
     result = ''
     for item in items:
         #Determine whether the item should be listed with 'a'/'an' or 'some'
@@ -67,4 +37,3 @@ def list_of_items(items):
         else:
             result += ', '
     return result.replace('_', ' ')
-return_items()
--- readmap.py
+++ readmap.py
@@ -1,6 +1,7 @@
 import xml.etree.ElementTree as ET
-converses = {'north':'south', 'east':'west', 'up':'down', 'left':'right', 'into':'out'}
-rooms = []
+import uuid
+converses = {'north':'south', 'east':'west', 'up':'down', 'left':'right', 'inside':'outside', 'upstairs':'downstairs'}
+rooms = {}
 
 def return_xml_items(room):
 	items = []
@@ -16,6 +17,13 @@ def return_xml_exits(room):
 	for exitdict in [room[i] for i in range(len(room)) if room[i].tag == 'exit']:
 		exits[exitdict.attrib['direction']] = exitdict.attrib['room']
 	return exits
+def return_xml_characters(room):
+	characters = []
+	for characterdict in [room[i] for i in range(len(room)) if room[i].tag == 'character']:
+		tempcharacter = {'name':characterdict.attrib['name'], 'description':characterdict.text}
+		tempcharacter['items'] = return_xml_items(characterdict)
+		characters.append(tempcharacter)
+	return characters
 def return_converse(text):
 	""" This function allows us to return the reverse of a direction 
 	such as North is the converse of South. It has to use the converses dictionary 
@@ -33,79 +41,35 @@ def return_converse(text):
 		return list(converses.keys())[list(converses.values()).index(text)]
 def fix_exits():
 	for room in rooms:
-		for direction, leads_to in room['exits'].items():
-			for roomtest in rooms:
-				if roomtest['name'] == leads_to and not return_converse(direction) in roomtest['exits'].keys():
-					roomtest['exits'][return_converse(direction)] = room['name']
+		for direction, leads_to in rooms[room]['exits'].items():
+			if not return_converse(direction) in rooms[leads_to]['exits']:
+				rooms[leads_to]['exits'][return_converse(direction)] = room
 def room_exists(room_id):
-	""" Checks to see if a room exists.
-	For example:
-	>>> room_exists('Reception')
-	True
-	"""
-	return room_id in [room['name'] for room in rooms]
+	return room_id in rooms.keys()
 def return_room(room_id):
 	if room_exists(room_id):
-		return list([room for room in rooms if room['name'] == room_id])[0]
-def find_room_by_exit(exit):
-	for room in rooms:
-		for direction, leads_to in exit.items():
-			if direction in room['exits']:
-				if leads_to in room['exits'][direction]:
-					return room
+		return rooms[room_id]
 def print_item_location(item_id):
 	for room in rooms:
 		for item in room['items']:
-			if item['name'].replace('_','') == item_id:
-				print('The ' + item['name'] + ' can be found in ' + (room['dir_name'] + ' ' if room['dir_name'] != '' else '') + room['name'])
+			if item['name'].replace('_','') == item_id and item['take']:
+				print('The ' + item['name'].replace('_', ' ') + ' can be found in ' + (room['dir_name'] + ' ' if room['dir_name'] != '' else '') + room['name'])
 				return
 	print('The \'' + item_id + '\' cannot be found')
-def return_room_from_exits(exits):
-	#
-	return list([room for room in rooms if room['exits'] == exits])[0]
-def return_rooms():
-	tree = ET.parse('map.xml')
-	xml = tree.getroot()
-	for room in xml:
+def return_rooms(xmlmap):
+	for room in xmlmap:
 		temproom = {'description': room.text}
-		for attributes, values in room.attrib.items():
-			temproom[attributes] = values
+		for attributes, value in room.attrib.items():
+			temproom[attributes] = (value if not (value.lower() == 'true') and not (value.lower() == 'false') else (True if value.lower() == 'true' else False))
 		temproom['exits'] = return_xml_exits(room)
 		temproom['items'] = return_xml_items(room)
-		rooms.append(temproom)
+		temproom['characters'] = return_xml_characters(room)
+		rooms[room.attrib['name']] = temproom
 	fix_exits()
 def exit_leads_to(exits, direction):
-	#
-	return return_room_from_exits(exits)['exits'][direction]
+	return exits[direction]
 def is_valid_exit(exits, chosen_exit):
-    """This function checks, given a dictionary "exits" (see map.py) and
-    a players's choice "chosen_exit" whether the player has chosen a valid exit.
-    It returns True if the exit is valid, and False otherwise. Assume that
-    the name of the exit has been normalised by the function normalise_input().
-    For example:
-
-    >>> is_valid_exit(rooms["Reception"]["exits"], "south")
-    True
-    >>> is_valid_exit(rooms["Reception"]["exits"], "up")
-    False
-    >>> is_valid_exit(rooms["Parking"]["exits"], "west")
-    False
-    >>> is_valid_exit(rooms["Parking"]["exits"], "east")
-    True
-    """
     return chosen_exit in exits
 def move_room(exits, direction):
-    """This function returns the room into which the player will move if, from a
-    dictionary "exits" of avaiable exits, they choose to move towards the exit
-    with the name given by "direction". For example:
+    return rooms[exits[direction]]
 
-    >>> move(rooms["Reception"]["exits"], "south") == rooms["Robs"]
-    True
-    >>> move(rooms["Reception"]["exits"], "east") == rooms["Tutor"]
-    True
-    >>> move(rooms["Reception"]["exits"], "west") == rooms["Office"]
-    False
-    """
-    return return_room(return_room_from_exits(exits)['exits'][direction])
-
-return_rooms()
